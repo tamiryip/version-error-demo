b9415446c4ba62a4acf2e40e37c3e86d85bc99b9
tamirye <tamirye@lexin.com>
2022-02-09 15:11:48 +0800

    feat: reproducing demo

---
+++ src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="index">
+    <view v-if="condition1 === true">
+      文案1
+    </view>
+    <view class="list-row">
+      默认文案
+    </view>
+    <view v-if="condition2 === true">
+      文案2
+    </view>
+  </view>
+</template>
+
+<script>
+import './index.less'
+
+export default {
+    data () {
+        return {
+            condition1: false,
+            condition2: false
+        }
+    },
+    created() {
+        this.init();
+    },
+    methods: {
+        init() {
+            setTimeout(() => {
+                this.condition1 = true;
+                this.condition2 = true;
+            }, 1000);
+        }
+    }
+}
+</script>
